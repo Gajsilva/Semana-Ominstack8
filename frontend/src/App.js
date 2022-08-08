870a33e6d136c782764da29ac9e115a6b9c3161e
Gabriel Silva <86989966+Gajsilva@users.noreply.github.com>
2022-08-08 00:56:14 -0300

    update

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Login from './pages/Login'
+
+
+export default function App() {
+  return (
+    <h1> Heloo world </h1>,
+    <Login />
+  );
+}
+
+
